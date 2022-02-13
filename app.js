0b06df0743a9ace781befacbde3fbb8aae8569ec
oraclerevolution <mac.n.drix@gmail.com>
2022-02-13 22:59:11 +0000

    CRUD books

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var moviesRouter = require('./routes/movies');
+var booksRouter = require('./routes/books');
 var cors = require('cors')
 
 
@@ -26,6 +27,7 @@ app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
