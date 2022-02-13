43ab992e2a6f956580a6c2cec03fbb6a81f97419
oraclerevolution <mac.n.drix@gmail.com>
2022-02-13 10:16:00 +0000

    ajout du package cors

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var moviesRouter = require('./routes/movies');
+var cors = require('cors')
+
 
 var app = express();
 
@@ -19,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
